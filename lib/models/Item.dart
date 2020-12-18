8c327daa0edb841cddc4bfed9dd46849958580e9
Dema323 <maxime.sateur@gmail.com>
2020-12-18 20:35:57 +0100

    changed filename

--- lib/models/Item.dart
+++ lib/models/Item.dart
@@ -1,22 +1,45 @@
 class Item {
-  String name;
-  double price;
+  String _name;
+  String _price;
+  String _size;
+  String _itemid;
 
-  Item(name, price);
+  Item(this._name, this._price, this._size);
+
+  Item.withid(this._name, this._price, this._size, this._itemid);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': _name,
+      'price': _price,
+      'size': _size,
+    };
+  }
+
+  Item.fromData(Map<String, dynamic> data)
+      : _name = data['name'],
+        _price = data['price'],
+        _size = data['size'];
+
+  static Item fromMap(Map<String, dynamic> map, String id) {
+    if (map == null) return null;
+
+    return Item.withid(map['name'], map['price'], map['size'], id);
+  }
 
   String getName() {
-    return name;
+    return this._name;
   }
 
-  double getPrice() {
-    return price;
+  String getPrice() {
+    return _price;
   }
 
-  void setName(name) {
-    this.name = name;
+  String getSize() {
+    return _size;
   }
 
-  void setPrice(price) {
-    this.price = price;
+  String getId() {
+    return _itemid;
   }
 }
